6c597123a977cc256f75b913519afa78ac12cf89
daniel <kimgooneya@gmail.com>
2023-07-11 23:36:48 +0900

    LESSON03 START

---
+++ LESSON03/build/js/main.js
@@ -0,0 +1,18 @@
+"use strict";
+let myName = 'Dave';
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'John';
+meaningOfLife = 42;
+isLoading = true;
+album = 5150;
+const sum = (a, b) => {
+    return a + b;
+};
+function sum2(a, b) {
+    return a + b;
+}
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ LESSON03/src/main.ts
@@ -0,0 +1,22 @@
+let myName:string = 'Dave'
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
+myName = 'John'
+meaningOfLife = 42
+isLoading = true
+album = 5150
+
+const sum = (a:number, b:number)=>{
+    return a + b
+}
+
+function sum2(a:number, b:number){
+    return a+b
+}
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re:RegExp = /\w+/g
